ead82103b3b5834da225a1d12c889cb93a674b29
Elua-b <bugingoeloi@gmail.com>
2026-03-11 12:50:28 +0200

    adding updates

--- app/invite/[code]/page.tsx
+++ app/invite/[code]/page.tsx
@@ -1,10 +1,9 @@
 import { InviteClient } from "./invite-client"
 
-
 // This is a Server Component
-export default function InvitePage({ params }: { params: { code: string } }) {
-  // Access params directly in the server component
-  const code = params.code
+export default async function InvitePage({ params }: { params: Promise<{ code: string }> }) {
+  // Await params in the server component for Next.js 15 compatibility
+  const { code } = await params
 
   return <InviteClient code={code} />
 }
